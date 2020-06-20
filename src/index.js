79e1d0ee17c56bbe97b80c4bea418207baf83e5d
tamaasrory <tamaasrory@gmail.com>
2020-06-20 13:45:51 +0700

    update router

--- src/index.js
+++ src/index.js
@@ -9,33 +9,21 @@ import "assets/scss/now-ui-kit.scss";
 import "assets/demo/demo.css";
 import "assets/demo/nucleo-icons-page-styles.css";
 // pages for this kit
-import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
 import LoginPage from "views/examples/LoginPage.js";
 import LandingPage from "views/examples/LandingPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
-import LandingPage1 from "views/page/LandingPage.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Switch>
-        <Route path="/index" render={props => <Index {...props} />} />
         <Route
-          path="/nucleo-icons"
-          render={props => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={props => <LandingPage {...props} />}
+            path="/index"
+            render={props => <LandingPage {...props} />}
         />
         <Route
           path="/profile-page"
           render={props => <ProfilePage {...props} />}
-        />
-         <Route
-          path="/landing-page1"
-          render={props => <LandingPage1 {...props} />}
         />
         <Route path="/login-page" render={props => <LoginPage {...props} />} />
         <Redirect to="/index" />
