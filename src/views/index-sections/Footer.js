2422d6df6196c488c0ae6cfabefb61eaa5c3695b
trimuerisandes <trimuerisandes@gmail.com>
2021-02-18 08:51:50 +0700

    footer #1

--- src/views/index-sections/Footer.js
+++ src/views/index-sections/Footer.js
@@ -9,28 +9,59 @@ function Footer() {
         <>
             <div className="separator separator-primary"></div>
             <div className="section section-story-overview bg-dark">
-                <div className="">
-                    <Container className="">
-                        <Row className=" ">
-                            <div className="ml-3 col-lg-2">
-                                <Col className="badge-light rounded-pill p-2">
+
+                <Container className="">
+                    <div className="">
+                        <Row className="col-lg-12 d-flex justify-content-center">
+                            <Col className="col-lg-4 pr-5">
+                                <div className="text-white">
+                                <div className=" badge-light rounded-pill p-2">
                                     <img
                                         alt="..."
                                         className=" img-fluid "
                                         src={require("assets/img/logo-tajriy.png")}
                                         width="50"
                                     ></img>
-                                </Col>
-                                <Col className="mt-5 text-white">
-                                    <h5>Kota Pekanbaru <br/>
-                                    Indonesia, 551525</h5>
+                                </div>
+                                    <h5 className="mt-5">Kota Pekanbaru <br/>
+                                        Indonesia, 551525</h5>
                                     <h5>Senin - Jum'at (08:00-17:00 WIB)</h5>
-                                </Col>
-                            </div>
+                                </div>
+                            </Col>
+                            <Col className="ml-5 text-left">
+                                <div>
+                                    <h5 className="text-white font-weight-bold">Tentang Kami</h5>
+                                    <div className="">
+                                        <h6 className="text-white text-capitalize mt-4">Portofolio</h6>
+                                        <h6 className="text-white text-capitalize">Blog</h6>
+                                        <h6 className="text-capitalize text-white">Syarat dan Ketentuan</h6>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col className="text-left ml-5">
+                                <div>
+                                    <h5 className="text-white font-weight-bold">Layanan</h5>
+                                    <div className="text-justify">
+                                    <h5 className="text-white mt-4">Sistem Informasi</h5>
+                                    <h5 className="text-white">Aplikasi Mobile</h5>
+                                    <h5 className="text-white">ERP</h5>
+                                    <h5 className="text-white">Company Profile</h5>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col className="text-left">
+                                <div>
+                                    <h5 className="text-white font-weight-bold">Marketing Info</h5>
+                                    <h5 className="text-white mt-4">081314721408 (Tri)</h5>
+                                    <h5 className="text-white">081314721408 (Aufa)</h5>
+                                    <h5 className="text-white">081314721408 (Tama)</h5>
+                                </div>
+                            </Col>
                         </Row>
-                    </Container>
-                </div>
+                    </div>
+                </Container>
             </div>
+
         </>
     );
 }
