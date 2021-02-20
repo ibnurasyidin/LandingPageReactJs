7b5d37c756733544bfbe4dccfcd8b2a5e5635901
trimuerisandes <trimuerisandes@gmail.com>
2021-02-21 02:55:46 +0700

    footer #2

--- src/views/index-sections/Footer.js
+++ src/views/index-sections/Footer.js
@@ -7,34 +7,35 @@ import {Button, Container, Row, Col} from "reactstrap";
 function Footer() {
     return (
         <>
-            <div className="separator separator-primary"></div>
-            <div className="section section-story-overview bg-dark">
-
+            <div className=" section-story-overview bg-dark">
                 <Container className="">
                     <div className="">
-                        <Row className="col-lg-12 d-flex justify-content-center">
+                        <Row className="section-well mt-5 bg-dark col-lg-12 ">
                             <Col className="col-lg-4 pr-5">
                                 <div className="text-white">
-                                <div className=" badge-light rounded-pill p-2">
-                                    <img
-                                        alt="..."
-                                        className=" img-fluid "
-                                        src={require("assets/img/logo-tajriy.png")}
-                                        width="50"
-                                    ></img>
-                                </div>
-                                    <h5 className="mt-5">Kota Pekanbaru <br/>
+                                        <Row>
+                                            <div>
+                                            <img
+                                                className="img-fluid"
+                                                src={require("assets/img/logo-white.png")}
+                                                width="60"
+                                            ></img>
+                                            </div>
+                                            <h2 className="font-weight-bold ml-3">Tajriy</h2>
+                                        </Row>
+
+                                    <h5 className="mt-3">Kota Pekanbaru <br/>
                                         Indonesia, 551525</h5>
                                     <h5>Senin - Jum'at (08:00-17:00 WIB)</h5>
                                 </div>
                             </Col>
-                            <Col className="ml-5 text-left">
+                            <Col className=" text-left">
                                 <div>
                                     <h5 className="text-white font-weight-bold">Tentang Kami</h5>
                                     <div className="">
-                                        <h6 className="text-white text-capitalize mt-4">Portofolio</h6>
-                                        <h6 className="text-white text-capitalize">Blog</h6>
-                                        <h6 className="text-capitalize text-white">Syarat dan Ketentuan</h6>
+                                        <h5 className="text-white text-capitalize mt-4">Portofolio</h5>
+                                        <h5 className="text-white text-capitalize">Blog</h5>
+                                        <h5 className="text-capitalize text-white">Syarat dan Ketentuan</h5>
                                     </div>
                                 </div>
                             </Col>
@@ -42,10 +43,10 @@ function Footer() {
                                 <div>
                                     <h5 className="text-white font-weight-bold">Layanan</h5>
                                     <div className="text-justify">
-                                    <h5 className="text-white mt-4">Sistem Informasi</h5>
-                                    <h5 className="text-white">Aplikasi Mobile</h5>
-                                    <h5 className="text-white">ERP</h5>
-                                    <h5 className="text-white">Company Profile</h5>
+                                        <h5 className="text-white mt-4">Sistem Informasi</h5>
+                                        <h5 className="text-white">Aplikasi Mobile</h5>
+                                        <h5 className="text-white">ERP</h5>
+                                        <h5 className="text-white">Company Profile</h5>
                                     </div>
                                 </div>
                             </Col>
