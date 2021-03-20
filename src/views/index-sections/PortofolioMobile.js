63bab7723c89052b7149bc763a276b5bd4120542
trimuerisandes <trimuerisandes@gmail.com>
2021-03-21 01:11:12 +0700

    for deploy bismilla

--- src/views/index-sections/PortofolioMobile.js
+++ src/views/index-sections/PortofolioMobile.js
@@ -10,7 +10,7 @@ function AndroidIos() {
         <>
 
             <div className="">
-                <h2 className="title text-center">Portofolio</h2>
+                {/*<h2 className="title text-center">Portofolio</h2>*/}
                 <Container className="">
                     <Row className="">
                         <Col sm={12} lg={6} md={5} className="order-lg-0 order-md-0 order-sm-0">
@@ -25,7 +25,7 @@ function AndroidIos() {
                             </div>
                         </Col>
                         <Col sm={12} md={7} lg={6} className="order-lg-1 order-md-1 align-self-center order-sm-1 mx-sm-2 mx-md-0 mx-lg-0">
-                            <h2 className="title title-padding-top text-right">Mobile Development <br/>Projects</h2>
+                            <h2 className="title  text-right">Mobile Development <br/>Projects</h2>
                             <h5 className="text-right">
                                 Lihat portofolio project kami dengan bebagai bisnis model
                             </h5>
