1f12409889b858d4992578d2394fc0f515315e7e
trimuerisandes <trimuerisandes@gmail.com>
2021-05-10 01:21:15 +0700

    add wp headless - new layout post - grid - done - image adjust - done - limit text

--- src/components/layouts/FeaturedImage.js
+++ src/components/layouts/FeaturedImage.js
@@ -6,9 +6,9 @@ const FeaturedImage = ( props ) => {
 	const { title } = props.title;
 
 	return (
-		<img width={ img_src[1] } height={ img_src[2] }
+		<img width={ img_src[1] } height={ img_src[225] }
 		     src={ img_src[0] }
-		     className="attachment-post-thumbnail size-post-thumbnail wp-post-image"
+		     className="attachment-post-thumbnail size-post-thumbnail wp-post-image container-fluid p-0 img-fluid"
 		     srcSet={ img_srcset ? img_srcset : '' }
 		     sizes={ img_sizes }
 		     alt={ title }
--- src/components/layouts/Postv2.js
+++ src/components/layouts/Postv2.js
@@ -11,16 +11,16 @@ export const Post = ( props ) => {
 
 						<div className="col-md-4">
 							<div className="card mb-4 shadow-sm">
-								<svg className="bd-placeholder-img card-img-top" width="100%" height="225"
+								{/*<svg className="bd-placeholder-img card-img-top" width="100%" height="225"
 									 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
-									 focusable="false" role="img" aria-label="Placeholder: Thumbnail">
-									<title>Placeholder</title>
-									<rect width="100%" height="100%" fill="#55595c"/>
-									<text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-								</svg>
+									 focusable="false" role="img" aria-label="Placeholder: Thumbnail">*/}
+									 <div  width="225" height="225">
+									{ post.attachment_image.img_sizes ? <FeaturedImage title={ post.title } image={ post.attachment_image }/> : '' }
+									 </div>
+								{/*</svg>*/}
 								<div className="card-body">
-									<p className="card-text">This is a wider card with supporting text below as a
-										natural lead-in to additional content. This content is a little bit longer.</p>
+									{ post.title ? <Link to={`/post/${ post.id }`} className="h4">{ post.title }</Link> : '' }
+									{ post.title ? <p className="post-excerpt mt-4" dangerouslySetInnerHTML={{__html: sanitize( post.excerpt )}}/> : '' }
 									<div className="d-flex justify-content-between align-items-center">
 										<div className="btn-group">
 											<button type="button" className="btn btn-sm btn-outline-secondary">View
