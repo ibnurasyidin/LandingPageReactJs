ef93820365cd29e0fca993090341ec814141402d
trimuerisandes <trimuerisandes@gmail.com>
2021-05-11 01:02:44 +0700

    add wp headless - new layout post - grid - done - image adjust - done - limit text( done by wordpress) - add meta post (done)

--- src/components/layouts/Postv2.js
+++ src/components/layouts/Postv2.js
@@ -16,6 +16,37 @@ export const Post = (props) => {
                         <FeaturedImage title={post.title} image={post.attachment_image}/> : ''}
                 </div>
                 <div className="card-body">
+                    {/*Post meta*/}
+                    <div className="post-meta">
+                        {/*Author*/}
+                        {/*<Link to={ `/author/${ post.meta.author_id }` } className="post-author">
+                            <i className="fa fa-user"/>
+                            <span>{ post.meta.author_name }</span>
+                        </Link>*/}
+                        {/*Date*/}
+                        <div className="post-date">
+                            {/*<i className="fa fa-clock-o"/>*/}
+                            <span>{ post.date }</span>
+                        </div>
+
+                        {/*	Categories*/}
+                        { post.categories.length ? (
+                            <div className="post-category">
+                                {/*<i className="fa fa-folder"/>*/}
+                                {
+                                    post.categories.map( ( category, index ) => {
+                                        return (
+                                            // If its not the last item.
+                                            index !== ( post.categories.length - 1 ) ?
+                                                <Link to={ `category/${ category.term_id }` } key={ category.term_id }>{ category.name }, </Link>
+                                                :
+                                                <Link to={ `category/${ category.term_id }` } key={ category.term_id }>{ category.name }</Link>
+                                        );
+                                    } )
+                                }
+                            </div>
+                        ) : '' }
+                    </div>
                     {post.title ? <Link to={`/post/${post.id}`} className="h4">{post.title}</Link> : ''}
                     {post.title ? <p className="post-excerpt mt-4"
                                      dangerouslySetInnerHTML={{__html: sanitize(post.excerpt)}}/> : ''}
